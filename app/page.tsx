be2cc178055deec9e1930fe88e07f63a33068c4d
Elisabete Costa <elisabete_costa94@hotmail.com>
2023-11-16 08:35:44 +0000

    add optimized images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+// For more info on font and image optimization use the recommended reads on the bottom of the page at https://nextjs.org/learn/dashboard-app/optimizing-fonts-images
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -29,7 +32,23 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* This image will only show on desktop */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+
+          {/* This image will only show on mobile */}
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+          />
         </div>
       </div>
     </main>
